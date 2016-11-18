1150417910520ce6675e0d5a702526e614529422
Pablo Sáez Parra <pablosaez1995@gmail.com>
2016-11-18 17:09:16 -0300

    funciones
    se agrego unas de las funciones principales con las que se va a trabajar a lo largo del trabajo

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-(function () {
+function addClass(clase, argument) {
+	//En esta clase se le ingresa el nombre de la clase que se va a modificar y
+	//en el argument va el nombre de la case del style que se incrustara al final
+	//del archivo
+	//SI SE VA A REEMPLAZAR UN ATRIBUTO YA ESTABLECIDO, FAVOR QUE AGREGAR "!important"
+	//A LOS ATRIBUTOS DEL STYLE A REEMPLAZAR
+	document.getElementById(clase).className += argument;
+}
 
-$(#main).addclass("black")
+function setClass(clase, argument) {
+	document.getElementById(clase).className = argument;
+}
 
-	})()
+function setHtml(clase, argument) {
+	document.getElementById(clase).innerHTML = argument;
+}
+
+function setImage(clase, argument) {
+	document.getElementById(clase).src = argument;
+}
+
+function function_name(argument) {
+	// body...
+}
